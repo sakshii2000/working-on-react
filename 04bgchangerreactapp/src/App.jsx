739f8d9f8ce24c5fcf5628baf264cc83f5877243
sakshii2000 <sakshi.bhakta2021@gmail.com>
2024-01-24 11:09:51 +0530

    colours simplified

--- 04bgchangerreactapp/src/App.jsx
+++ 04bgchangerreactapp/src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
 import ButtonColor from "./ButtonColor";
-import { useEffect } from "react";
 
 function App() {
   const [colour, setColour] = useState("olive");
+
+  const colours = [
+    "red",
+    "green",
+    "blue",
+    "olive",
+    "gray",
+    "yellow",
+    "pink",
+    "purple",
+    "lavender",
+    "white",
+    "black"
+  ];
 
   return (
     <div
@@ -11,50 +24,13 @@ function App() {
       style={{ backgroundColor: colour }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
-          <ButtonColor
-            color="red"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="green"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="blue"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="olive"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="gray"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="yellow"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="pink"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="purple"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="lavender"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="white"
-            setColour={setColour}
-          />
-          <ButtonColor
-            color="black"
-            setColour={setColour}
-          />
+          {colours.map((colour) => (
+            <ButtonColor
+              key={colour}
+              color={colour}
+              setColour={setColour}
+            />
+          ))}
         </div>
       </div>
     </div>
